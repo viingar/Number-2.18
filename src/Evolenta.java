fdb6648335f1548ff2affc4575bab7fc0120572f
viingar <myuniverse8082@gmail.com>
2023-10-26 22:09:38 +0300

    123

---
+++ src/Evolenta.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Evolenta{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Как тебя зовут?");
+        String name = in.nextLine ();
+        System.out.println("Привет " + name );
+
+
+
+
+    }
+}
